1768c179f59b181646ecfe9e3872521f86f3b372
Vishal Chavare <chavarevishal04@gmail.com>
2026-06-27 06:03:16 -0500

    initial commit

---
+++ PRIMARY KEY.sql
@@ -0,0 +1,28 @@
+﻿create database SQL3YEO
+
+use SQL3YEO
+
+create table customer(
+		custID int primary key,
+		custName varchar(100)
+		)
+
+select *from customer
+
+
+insert into customer values(1,'vshal');
+
+🔑 PRIMARY KEY
+
+👉 प्रत्येक row ला unique ओळख देतो
+👉 NULL नसतो
+👉 duplicate नसतो
+
+🏦 Insurance Example:
+CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY,
+    Name VARCHAR(100)
+);
+
+📌 Meaning:
+👉 प्रत्येक customer unique
