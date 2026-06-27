dc10403feec193e33c10ce3c2b635fb684448ece
Vishal Chavare <chavarevishal04@gmail.com>
2026-06-27 07:05:58 -0500

    DeleteDropTrunket

---
+++ DELETE vs TRUNCATE vs DROP.sql
@@ -0,0 +1,22 @@
+﻿DELETE vs TRUNCATE vs DROP
+=================================================================================
+🔴 1. DELETE (Row-level remove)
+👉 Specific record delete करतो
+
+Example:
+DELETE FROM Customer WHERE CustomerID = 1;
+========================================================================
+
+⚡ 2. TRUNCATE (Full table fast empty)
+👉 सगळा data delete होतो पण structure राहतो
+
+Example:
+TRUNCATE TABLE Customer;
+=================================================================================
+
+. 3.DROP (Complete remove)
+👉 Table पूर्ण उडवतो (data + structure)
+
+Example:
+DROP TABLE Customer;
+=================================================================================
