c00ec96ce949a73baa0ed12b3c1e7cc64931e54f
Vishal Chavare <chavarevishal04@gmail.com>
2026-06-27 06:55:50 -0500

    DENormalization

---
+++ Denormalization.sql
@@ -0,0 +1,25 @@
+﻿🧠 What is Denormalization?
+
+👉 Denormalization म्हणजे normalization च्या उलट process
+👉 यात आपण intentional duplicate data ठेवतो for performance + fast reporting
+
+
+
+⚖️ Normalization vs Denormalization
+Feature					Normalization					Denormalization
+Data duplication		❌ No								✔ Yes
+Performance (read)		Slow								Fast
+Data integrity				High							Medium
+Use case				OLTP (transactions)					OLAP (reports)
+
+🚀 Real Company Use (VERY IMPORTANT)
+💼 Where Denormalization is used:
+1. 📊 Power BI / Reporting systems
+Daily dashboards
+KPI reports
+2. 🏦 Banking / Insurance analytics
+Claim trends
+Premium analysis
+3. ⚡ Data Warehouses (DW)
+Snowflake / Star schema
+OLAP systems
