20470ac1241f1be982e4e9b1db3c0979ab310591
Vishal Chavare <chavarevishal04@gmail.com>
2026-06-27 07:19:23 -0500

    Store Procedure

---
+++ Stored Procedure.sql
@@ -0,0 +1,25 @@
+﻿Stored Procedure
+
+👉 Stored Procedure म्हणजे pre-written SQL logic
+👉 ज्यामध्ये आपण INSERT / UPDATE / DELETE / SELECT करू शकतो
+
+create procedure GetCustomerPolicies
+
+@CustomerID INT
+AS
+Begin 
+	Select *
+		 from CustomerPolicy
+		 where CustomerID=@CustomerID;
+		 End;
+
+		 EXEC GetCustomerPolicies 1;
+
+
+
+💡 Features:
+
+✔ Multiple SQL statements allowed
+✔ DML operations (INSERT/UPDATE/DELETE) allowed
+✔ Input parameters घेऊ शकतो
+✔ Performance better (precompiled)
