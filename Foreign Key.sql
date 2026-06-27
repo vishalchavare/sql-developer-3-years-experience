4a93aa2d910ecc0925e8be006964a9b3fa1702e6
Vishal Chavare <chavarevishal04@gmail.com>
2026-06-27 06:33:19 -0500

    Foreign key

---
+++ Foreign Key.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE Policy (
+    PolicyID INT PRIMARY KEY
+);
+
+CREATE TABLE CustomerPolicy (
+    CustomerPolicyID INT PRIMARY KEY,
+    CustomerID INT,
+    PolicyID INT,
+    FOREIGN KEY (CustomerID) REFERENCES Customer1(CustomerID),
+    FOREIGN KEY (PolicyID) REFERENCES Policy(PolicyID)
+);
+
+CREATE TABLE Customer1 (
+    CustomerID INT PRIMARY KEY,
+    FullName VARCHAR(100)
+);
+
+
+select *from CustomerPolicy
+
+Foreign Key म्हणजे असा column जो दुसऱ्या table च्या Primary Key ला reference करतो
+👉 तो tables मध्ये relationship (नातं) तयार करतो
+
+🧠 Simple Meaning
+
+👉 CustomerPolicy table सांगतो:
+कोणत्या customer ने कोणती policy घेतली आहे
+
+🏆 One Line Summary
+
+👉 Foreign Key = “Link between two tables that maintains data consistency”
